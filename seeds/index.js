72a444b33717c7f1367a27b6b22ca6804f2d2f7d
Mariano Mossburg <mariano_mossburg@outlook.com>
2023-12-15 19:16:06 -0300

    first commit

--- seeds/index.js
+++ seeds/index.js
@@ -22,9 +22,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Curabitur convallis, justo ut suscipit hendrerit, ligula est malesuada odio. Quisque vel massa ut arcu ultrices vestibulum. Integer at dui sed metus feugiat hendrerit. Nam nec dapibus nisi, vel fringilla ipsum.',
+            price
         })
         await camp.save();
 
