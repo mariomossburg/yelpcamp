86501270f599645ff73ee4acbe2281f762de0fc9
Mariano Mossburg <marmossburg@gmail.com>
2023-12-21 15:18:06 -0300

    First commit and contribution test

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const eJsMate = require('ejs-mate');
-const methodOverride = require('method-override')
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp',{
@@ -47,27 +49,28 @@ app.get('/campgrounds/new', (req,res) =>{
     res.render('campgrounds/new');
 });
 
-app.post('/campgrounds', async(req,res) =>{
+app.post('/campgrounds', catchAsync(async(req,res) =>{
+    if(!req.body.Campground) throw new ExpressError('Invalid Campground Data', 404);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
-})
+}))
 
-app.get('/campgrounds/:id', async (req,res) => {
+app.get('/campgrounds/:id', catchAsync(async (req,res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/show', { campground });
-});
+}));
 
-app.get('/campgrounds/:id/edit', async(req,res) => {
+app.get('/campgrounds/:id/edit', catchAsync(async(req,res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/edit', { campground });
-})
+}))
 
-app.put('/campgrounds/:id', async(req,res) => {
+app.put('/campgrounds/:id', catchAsync(async(req,res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground});
     res.redirect(`/campgrounds/${campground._id}`)
-})
+}))
 
 app.delete('/campgrounds/:id', async (req,res) => {
     const {id} = req.params;
@@ -75,6 +78,14 @@ app.delete('/campgrounds/:id', async (req,res) => {
     res.redirect('/campgrounds');
 })
 
+app.all('*', (req,res,next)=>{
+    next(new ExpressError('Page Not Found', 404))
+})
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500, message = 'something went wrong' } = err;
+    res.status(statusCode).send(message)
+})
 
 app.listen(3000, () => {
     console.log('serving in port 3000')
