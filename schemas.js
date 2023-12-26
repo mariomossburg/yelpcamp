cb422314f6fa048be910e9b11d1b87b0a85dd48d
Mariano Mossburg <marmossburg@gmail.com>
2023-12-26 19:41:37 -0300

    review

--- schemas.js
+++ schemas.js
@@ -1,8 +1,8 @@
 const Joi = require('joi');
 module.exports.campgroundSchema = Joi.object({
     campground: Joi.object({
-        title: Joi.string().require(),
-        price: Joi.number().required.min(0),
+        title: Joi.string().required(),
+        price: Joi.number().required().min(0),
         image: Joi.string().required(),
         location: Joi.string().required(),
         description: Joi.string().required()
