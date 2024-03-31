6279481e8328a5d493b5493f6bb86429849b8612
Mariano Mossburg <marmossburg@gmail.com>
2024-03-31 20:34:46 -0300

    styling

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -4,7 +4,7 @@ const { text } = require("express");
 
 	mapboxgl.accessToken = mapToken;
     const map = new mapboxgl.Map({
-        container: 'map',
+        container: 'cluster-map',
         // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
         style: 'mapbox://styles/mapbox/dark-v11',
         center: [-103.5917, 40.6699],
